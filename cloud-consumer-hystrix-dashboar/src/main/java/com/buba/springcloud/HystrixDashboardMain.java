afe4db7d399fb6afbc82e76f0bd06c1cd6d5478f
ming.li <ming.li@goupwith.com>
2023-07-28 15:40:14 +0800

    hystrix开发

---
+++ cloud-consumer-hystrix-dashboar/src/main/java/com/buba/springcloud/HystrixDashboardMain.java
@@ -0,0 +1,17 @@
+package com.buba.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+/**
+ * @author ming.li
+ * @date 2023/7/28 14:41
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain.class,args);
+    }
+}
